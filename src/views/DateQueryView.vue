d5aadc3c0b5b278cf32f48631da9474f061c36f4
DESKTOP-1AKMAGM\Lucas <tdksr0505@gmail.com>
2023-02-01 22:02:36 +0800

    fix: 修正日期查詢頁面 日期無顯示問題

--- src/views/DateQueryView.vue
+++ src/views/DateQueryView.vue
@@ -17,7 +17,7 @@ const getTodayDate = () => {
   return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(
     2,
     "0"
-  )}-${date.getDate()}`;
+  )}-${String(date.getDate()).padStart(2, "0")}`;
 };
 
 const dateQueryData = reactive<TDateQueryData>({

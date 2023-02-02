dc938005440ee7cd4ece059045b0cce85a5f79d3
DESKTOP-1AKMAGM\Lucas <tdksr0505@gmail.com>
2023-02-02 20:57:49 +0800

    feat: 新增rwd

--- src/views/DateQueryView.vue
+++ src/views/DateQueryView.vue
@@ -90,40 +90,45 @@ watch(
 </script>
 
 <template>
-  <div>
-    <n-date-picker
-      v-model:formatted-value="pickDate"
-      value-format="yyyy-MM-dd"
-      type="date"
-      size="large"
-    />
-  </div>
-  <div class="flex items-center mt-3">
-    <div class="flex-1">
-      <n-select
-        v-model:value="fromStation"
-        :options="stationInfoStore.getStaionsSelectOption"
+  <div class="md:flex md:items-center">
+    <div class="md:w-1/4 md:pr-4">
+      <n-date-picker
+        v-model:formatted-value="pickDate"
+        value-format="yyyy-MM-dd"
+        type="date"
         size="large"
       />
     </div>
-    <div class="flex-initial px-2">～</div>
-    <div class="flex-1">
-      <n-select
-        v-model:value="toStation"
-        :options="stationInfoStore.getStaionsSelectOption"
-        size="large"
-      />
+    <div class="flex items-center mt-3 md:mt-0 md:w-1/2 md:pr-4">
+      <div class="flex-1">
+        <n-select
+          v-model:value="fromStation"
+          :options="stationInfoStore.getStaionsSelectOption"
+          size="large"
+        />
+      </div>
+      <div class="flex-initial px-2">～</div>
+      <div class="flex-1">
+        <n-select
+          v-model:value="toStation"
+          :options="stationInfoStore.getStaionsSelectOption"
+          size="large"
+        />
+      </div>
     </div>
-  </div>
-  <div class="mt-3 h-9 text-center">
-    <n-button @click="onClickExchange" class="w-full h-full" type="info">
-      <ArrowsLeftRight class="w-7" />
-    </n-button>
-  </div>
-  <div class="mt-3">
-    <n-button @click="onClickQuery" class="w-full text-base" type="primary"
-      >查詢</n-button
-    >
+    <div class="mt-3 h-9 text-center md:mt-0 md:w-[100px] md:mr-4">
+      <n-button @click="onClickExchange" class="w-full h-full" type="info">
+        <ArrowsLeftRight class="w-7" />
+      </n-button>
+    </div>
+    <div class="mt-3 md:mt-0 md:w-[100px]">
+      <n-button
+        @click="onClickQuery"
+        class="w-full text-base h-9"
+        type="primary"
+        >查詢</n-button
+      >
+    </div>
   </div>
   <div v-if="dateQueryData.tableData" class="pt-4">
     <DateQueryTable :data="dateQueryData"></DateQueryTable>
